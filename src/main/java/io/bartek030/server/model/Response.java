7ba396a11f4f361c884874ff4db6d92f518047c4
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-04-07 22:26:15 +0200

    Custom http response

---
+++ src/main/java/io/bartek030/server/model/Response.java
@@ -0,0 +1,24 @@
+package io.bartek030.server.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
+
+@Data
+@SuperBuilder // Builder pattern
+@JsonInclude(NON_NULL) // Including only not-null values
+public class Response {
+    protected LocalDateTime timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
+}
