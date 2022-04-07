7f017d05743bf6b77f646a0f3d871428c1195a83
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-04-07 22:19:05 +0200

    Service implementing

--- src/main/java/io/bartek030/server/service/ServerService.java
+++ src/main/java/io/bartek030/server/service/ServerService.java
@@ -2,11 +2,13 @@ package io.bartek030.server.service;
 
 import io.bartek030.server.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server pingServer(String ipAddress);
+    Server pingServer(String ipAddress) throws IOException;
     Collection<Server> serverList(int limit);
     Server getServer(Long id);
     Server updateServer(Server server);
